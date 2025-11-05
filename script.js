c8d7b2a9123aca1f79ffa666b5435eec81dc0082
MariahAnnette <Mariahannette08@outlook.com>
2025-11-05 21:00:56 +0000

    local storage progress

--- script.js
+++ script.js
@@ -14,6 +14,40 @@ function addTask() {
     let taskText = textbox.value;
     textbox.value = "";
 
+    createTask(taskText);
+}
+
+function removeTask(event) {
+    // get the id of div to remove
+    let checkboxId = event.target.id; 
+    let idNum = checkboxId.substring(8);
+    let taskDiv = document.getElementById("task" + idNum);
+    taskDiv.classList.add("remove");
+    //get the task-list container
+    let taskList= document.getElementById("task-list");
+ 
+    //remove task div from task list
+    setTimeout(function() {
+        taskList.removeChild(taskDiv);
+        fixTaskColors();  
+    }, 2000)
+    
+ 
+   
+ 
+} 
+
+function fixTaskColors(){
+    //get the task-list container
+    let taskList= document.getElementById("task-list");
+    for (let i = 0; i < taskList.childElementCount; i++){
+        taskList.children[i].style.backgroundColor = "dodgerblue";
+        if (i % 2 == 1) {
+            taskList.children[i].style.backgroundColor = "pink";
+        }
+    }
+}
+function createTask(taskText) {
     // Get tasklist 
     let taskList = document.getElementById("task-list");
 
@@ -40,6 +74,10 @@ function addTask() {
     // Set label text
     label.innerText = taskText;
 
+    //save task to local storage
+    localStorage.setItem(taskDiv.id, taskText);
+    console.log(localStorage.length)
+
     // Add the checkbox the task div
     taskDiv.appendChild(checkbox);
 
@@ -50,33 +88,15 @@ function addTask() {
     taskList.appendChild(taskDiv);
 
 }
-
-function removeTask(event) {
-    // get the id of div to remove
-    let checkboxId = event.target.id; 
-    let idNum = checkboxId.substring(8);
-    let taskDiv = document.getElementById("task" + idNum);
-    taskDiv.classList.add("remove");
-    //get the task-list container
-    let taskList= document.getElementById("task-list");
-
-    //remove task div from task list
-    setTimeout(function() {
-        taskList.removeChild(taskDiv);
-        fixTaskColors();  
-    }, 2000)
-    
-
-   
-
-}
-function fixTaskColors(){
-    //get the task-list container
-    let taskList= document.getElementById("task-list");
-    for (let i = 0; i < taskList.childElementCount; i++){
-        taskList.children[i].style.backgroundColor = "dodgerblue";
-        if (i % 2 == 1) {
-            taskList.children[i].style.backgroundColor = "pink";
-        }
+function loadTasks(){
+    console.log(localStorage.length);
+    for(let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        console.log(key);
+        let taskText = localStorage.getItem(key);
+        console.log(taskText);
+        createTask(taskText);
     }
 }
+
+loadTasks();
